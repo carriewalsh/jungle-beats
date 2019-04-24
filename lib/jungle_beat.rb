387b7dfd27fe08d28b332ce016c3052156e9880e
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-04-24 12:46:52 -0600

    Adds method for append for jb

--- lib/jungle_beat.rb
+++ lib/jungle_beat.rb
@@ -8,4 +8,19 @@ class JungleBeat
   def count
     @list.count
   end
+
+  def append(string)
+    current_string = string
+    word = ""
+    until current_string.length == 0
+      if current_string.chr != " "
+        word += string.chr
+      else
+        @list.append(word)
+        word = ""
+      end
+      current_string[0] = ""
+    end
+    @list.append(word)
+  end
 end
