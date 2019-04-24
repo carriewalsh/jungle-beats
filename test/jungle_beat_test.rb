68a1c90f2f3e681dd7f5512c8548d90ba8265e7b
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-04-24 12:24:30 -0600

    Fixes test_helper require

--- test/jungle_beat_test.rb
+++ test/jungle_beat_test.rb
@@ -1,1 +1,12 @@
-require "test_helper"
+require "./test/test_helper"
+
+class JungleBeatTest < Minitest::Test
+  def setup
+    @jungle_beat = JungleBeat.new
+  end
+
+  def test_jungle_beat_exists
+    assert_instance_of JungleBeat, @jungle_beat
+  end
+
+end
