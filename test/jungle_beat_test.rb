d3877511f55f05b7ddd47b5e931453f78bd1431e
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-04-24 12:29:07 -0600

    Adds initial tests for jungle beat

--- test/jungle_beat_test.rb
+++ test/jungle_beat_test.rb
@@ -9,4 +9,8 @@ class JungleBeatTest < Minitest::Test
     assert_instance_of JungleBeat, @jungle_beat
   end
 
+  def test_jungle_beat_starts_with_empty_list
+    assert @jungle_beat.list
+    assert_nil @jungle_beat.list.head
+  end
 end
